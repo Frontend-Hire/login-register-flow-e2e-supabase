d1e0e6dec680a2b090f4550fe5f135c587dcccbc
yaralahruthik <yaralahruthikreddy@gmail.com>
2024-05-08 00:45:36 +0530

    landing page

---
+++ src/app/(marketing)/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <section className="container py-2">
+      <h1 className="text-3xl font-bold">Welcome to our website</h1>
+      <p className="text-gray-700">This is the landing page</p>
+    </section>
+  );
+}
